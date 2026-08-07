f700842408fc0a1cc1cfea73e2aa943c56d14c8e
Santino Ventrice <santino.ventrice@gmail.com>
2026-08-07 03:08:28 -0300

    chores: agregando allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  allowedDevOrigins: process.env.ALLOWED_DEV_ORIGINS 
+    ? process.env.ALLOWED_DEV_ORIGINS.split(',') 
+    : ['localhost:3000'],
+}
+
 export default nextConfig;
