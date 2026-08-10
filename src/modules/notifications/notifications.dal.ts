60ecaaa454ae309e0e1b3c83b58a1aa0a2f10e39
Santino Ventrice <santino.ventrice@gmail.com>
2026-08-10 02:38:22 -0300

    feat: agregando fetch de notificaciones de conductor

---
+++ src/modules/notifications/notifications.dal.ts
@@ -0,0 +1,34 @@
+import { getServerClient } from "@/utils/supabase/getServerClient"
+
+export interface Notification {
+    id: string;
+    user_id: string;
+    type: string;
+    message: string;
+    seen: boolean;
+    created_at: string;
+}
+
+export const getNotifications = async (userId: string) => {
+    const supabase = await getServerClient();
+
+    const {data, error} = await supabase
+    .from("notifications")
+    .select(`
+        id,
+        user_id,
+        type,
+        message,
+        seen,
+        created_at
+    `)
+    .eq("user_id", userId)
+    .order("created_at", {ascending: false});
+
+    if (error) {
+        console.error("Error trayendo las notificaciones del conductor. ", error);
+        return [];
+    }
+
+    return data ?? [];
+}
